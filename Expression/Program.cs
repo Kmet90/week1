2020dc5fda50399bcfcb20356b62b631f2639142
Kmet90 <kmet90@gmail.com>
2017-09-22 15:58:05 +0100

    More

--- Expression/Program.cs
+++ Expression/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloSoftUni
+namespace Expression
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello SoftUni");
+            Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
         }
     }
 }
